ebaff5a7fbf5de953e8d236675892a2718d1db45
certainly <j1o2b3@gmail.com>
2017-03-21 10:50:45 +0800

    first commit

---
+++ Caculator/Caculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Caculator
+//
+//  Created by certainly on 2017/3/20.
+//  Copyright © 2017年 certainly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInMiddleOfTyping = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text =  digit
+            userIsInMiddleOfTyping = true
+        }
+
+    }
+
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CaculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+}
+
