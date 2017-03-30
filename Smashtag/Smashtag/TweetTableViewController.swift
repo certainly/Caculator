b2f102ce0a5a767ab03d6860da95676f29fd7c82
certainly <j1o2b3@gmail.com>
2017-03-30 19:39:18 +0800

    L9

---
+++ Smashtag/Smashtag/TweetTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by certainly on 2017/3/30.
+//  Copyright © 2017年 certainly. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
+
+
+    // part of our Model
+    // each sub-Array of Tweets is another "pull" from Twitter
+    // and corresponds to a section in our table
+    private var tweets = [Array<Twitter.Tweet>]()
+    
+    var searchText: String? {
+        didSet {
+            searchTextField?.text = searchText
+            searchTextField?.resignFirstResponder()
+            lastTwitterRequest = nil                // REFRESHING
+            tweets.removeAll()
+            tableView.reloadData()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func twitterRequest() -> Twitter.Request? {
+        if let query = searchText, !query.isEmpty {
+            return Twitter.Request(search: query, count: 100)
+        }
+        return nil
+    }
+    
+    private func searchForTweets() {
+        if let request = twitterRequest() {
+            lastTwitterRequest = request
+            request.fetchTweets({ [weak self] newTweets in
+                DispatchQueue.main.async {
+                    
+                    if request == self?.lastTwitterRequest {
+                        self?.tweets.insert(newTweets, at: 0)
+                        self?.tableView.insertSections([0], with: .fade)
+                    }
+                    self?.refreshControl?.endRefreshing()
+                }
+            })
+        } else {
+            self.refreshControl?.endRefreshing() 
+        }
+    }
+    
+    @IBAction func refresh(_ sender: UIRefreshControl) {
+        searchForTweets()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+//        searchText = "kemono"
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField! {
+        didSet {
+           searchTextField.delegate = self
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            searchText = searchTextField.text
+        }
+        return true
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return tweets.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweets[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Tweet", for: indexPath)
+        let tweet = tweets[indexPath.section][indexPath.row]
+        if let tweetCell = cell as? TweetTableViewCell {
+            tweetCell.tweet = tweet
+        }
+//        cell.textLabel?.text = tweet.text
+//        cell.detailTextLabel?.text = tweet.user.name 
+        return cell
+    }
+
+   
+}
